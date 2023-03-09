91d65e19b3334bacb994c4d6c5833cc29a8c2d45
vluve <vladislav.naidyonov@yandex.ru>
2023-03-09 17:46:57 +0300

    added logcat outputs
    ADDED: all activity methods to MainActivity and DetailActivity
    ADDED: Logs to what methods were called in MainActivity and DetailActivity

--- app/src/main/java/com/vluve/helloandroid/DetailActivity.java
+++ app/src/main/java/com/vluve/helloandroid/DetailActivity.java
@@ -3,12 +3,45 @@ package com.vluve.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.w("DEBUG:", "DetailActivity onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.w("DEBUG:", "DetailActivity onStart() called");
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        Log.w("DEBUG:", "DetailActivity onResume() called");
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        Log.w("DEBUG:", "DetailActivity onPause() called");
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        Log.w("DEBUG:", "DetailActivity onStop() called");
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        Log.w("DEBUG:", "DetailActivity onDestroy() called");
+        super.onDestroy();
+        // The activity is about to be destroyed.
     }
 }
--- app/src/main/java/com/vluve/helloandroid/MainActivity.java
+++ app/src/main/java/com/vluve/helloandroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.w("DEBUG:", "MainActivity onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -32,6 +34,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        Log.w("DEBUG:", "MainActivity onStart() called");
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        Log.w("DEBUG:", "MainActivity onResume() called");
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        Log.w("DEBUG:", "MainActivity onPause() called");
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        Log.w("DEBUG:", "MainActivity onStop() called");
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        Log.w("DEBUG:", "MainActivity onDestroy() called");
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
 
     protected void DetailButtonClicked() {
         Intent intent = new Intent(this, DetailActivity.class);
